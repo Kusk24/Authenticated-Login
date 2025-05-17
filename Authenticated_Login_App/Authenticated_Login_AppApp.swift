e8eb3febe013e08acf923d4b9b307fc1813db9d7
Kusk24 <winyumg2003a@gmail.com>
2025-05-17 12:24:26 +0700

    Initialized Swift Ui App

---
+++ Authenticated_Login_App/Authenticated_Login_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Authenticated_Login_AppApp.swift
+//  Authenticated_Login_App
+//
+//  Created by Win Yu Maung on 17/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Authenticated_Login_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
